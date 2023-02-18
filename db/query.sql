ebf7caf1a813ec4e3d722be0aec6b3ee1db1ee23
mqas1 <114903874+mqas1@users.noreply.github.com>
2023-02-18 14:18:32 +1100

    adds query to gitignore

--- db/query.sql
+++ db/query.sql
@@ -4,7 +4,7 @@ SELECT employee.id, employee.first_name, employee.last_name, role.title, departm
 FROM employee
 JOIN role ON employee.role_id = role.id
 JOIN department ON role.department_id = department.id
-JOIN employee manager ON employee.manager_id = manager.id;
--- WHERE manager.manager_id IS NULL OR employee.manager_id IS NOT NULL;
+LEFT JOIN employee manager ON employee.manager_id = manager.id
+ORDER BY employee.last_name;
 
 SELECT * FROM employee;
