b14f9418c8317685f2160d3f3eca8dc2c6a59b70
mqas1 <114903874+mqas1@users.noreply.github.com>
2023-02-18 17:20:54 +1100

    adds constructor function with preliminary functionality

--- db/query.sql
+++ db/query.sql
@@ -1,10 +1,14 @@
 USE employee_db;
 
+SELECT * FROM department
+ORDER BY department.name;
+
+SELECT role.id, role.title, department.name AS department, role.salary
+FROM role
+JOIN department ON role.department_id = department.id;
+
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
 FROM employee
 JOIN role ON employee.role_id = role.id
 JOIN department ON role.department_id = department.id
-LEFT JOIN employee manager ON employee.manager_id = manager.id
-ORDER BY employee.last_name;
-
-SELECT * FROM employee;
+LEFT JOIN employee manager ON employee.manager_id = manager.id;
